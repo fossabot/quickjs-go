4f6085830333bca48feae3443bf3858a23f9a3f4
wangbuke <wangbuke@gmail.com>
2022-06-07 14:58:02 +0800

    make it run

---
+++ quickjs.go
@@ -0,0 +1,11 @@
+/*
+Package quickjs Go bindings to QuickJS: a fast, small, and embeddable ES2020 JavaScript interpreter
+*/
+package quickjs
+
+/*
+#cgo CFLAGS: -I./deps/vendor/github.com/koush/quickjs
+#cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/deps/libs/darwin_amd64 -lquickjs
+
+*/
+import "C"
