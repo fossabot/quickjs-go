ee8cc8accb194fc3de6ef71ff3fb2df1eced6663
wangbuke <wangbuke@gmail.com>
2022-06-08 01:29:14 +0800

    set CGO LDFLAGS and CFLAGS

--- quickjs.go
+++ quickjs.go
@@ -4,8 +4,12 @@ Package quickjs Go bindings to QuickJS: a fast, small, and embeddable ES2020 Jav
 package quickjs
 
 /*
+#cgo CFLAGS: -D_GNU_SOURCE
+#cgo CFLAGS: -DCONFIG_BIGNUM
+#cgo CFLAGS: -DEMSCRIPTEN
 #cgo CFLAGS: -I./deps/include
+#cgo LDFLAGS: -lm
 #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/deps/libs/darwin_amd64 -lquickjs
-#cgo linux,x86_64 LDFLAGS: -L${SRCDIR}/deps/libs/linux_x86_64 -lquickjs
+#cgo linux,amd64 LDFLAGS: -L${SRCDIR}/deps/libs/linux_amd64 -lquickjs
 */
 import "C"
