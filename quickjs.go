5dd892d37970e020bb8ed990c03db7f6e74dea9a
wangbuke <wangbuke@gmail.com>
2022-06-07 18:07:17 +0800

    add linux_x86_64 support

--- quickjs.go
+++ quickjs.go
@@ -6,5 +6,6 @@ package quickjs
 /*
 #cgo CFLAGS: -I./deps/include/quickjs
 #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/deps/libs/darwin_amd64 -lquickjs
+#cgo linux,x86_64 LDFLAGS: -L${SRCDIR}/deps/libs/linux_x86_64 -lquickjs
 */
 import "C"
