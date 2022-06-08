f41b3358984c11da46266c71bbcefba62269e1b2
wangbuke <wangbuke@gmail.com>
2022-06-09 03:20:26 +0800

    add windows build

--- quickjs.go
+++ quickjs.go
@@ -4,13 +4,13 @@ Package quickjs Go bindings to QuickJS: a fast, small, and embeddable ES2020 Jav
 package quickjs
 
 /*
-#cgo CFLAGS: -D_GNU_SOURCE
-#cgo CFLAGS: -DCONFIG_BIGNUM
-#cgo CFLAGS: -DEMSCRIPTEN
 #cgo CFLAGS: -I./deps/include
 #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/deps/libs/darwin_amd64 -lquickjs -lm
 #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/deps/libs/darwin_arm64 -lquickjs -lm
 #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/deps/libs/linux_amd64 -lquickjs -lm
 #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/deps/libs/linux_arm64 -lquickjs -lm
+#cgo windows_amd64 LDFLAGS: -L${SRCDIR}/deps/libs/windows_amd64 -lquickjs -lm
+#cgo windows,386 LDFLAGS: -L${SRCDIR}/deps/libs/windows_386 -lquickjs -lm
+
 */
 import "C"
